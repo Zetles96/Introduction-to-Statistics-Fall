be8b38f608da537beb17a0ca5db1351f4ea4f9f1
Mads Legard Nielsen <90190656+Zetles96@users.noreply.github.com>
2022-11-07 17:57:30 +0100

    asd

---
+++ soenderborg2/dataHandlingSoenderborg2.R
@@ -0,0 +1,3 @@
+df <- read.csv("C:\\Users\\madsn\\Documents\\Git\\Introduction-to-Statistics-Fall\\soenderborg2\\soenderborg2_data.csv",
+               header = TRUE, stringsAsFactors = FALSE, sep = ";")
+summary(df)
