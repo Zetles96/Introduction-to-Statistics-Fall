e589c2e9162b52b46e48d874ee4ed5f178b04d9f
Mads Nielsen <90190656+Zetles96@users.noreply.github.com>
2022-11-09 20:02:51 +0100

    no more assignment

--- soenderborg2/dataHandlingSoenderborg2.R
+++ soenderborg2/dataHandlingSoenderborg2.R
@@ -78,4 +78,20 @@ qqnorm(fit$residuals, ylab = "Residuals", xlab = "Z-scores",
 qqline(fit$residuals)
 
 #  condfidence interval
+qt(0.975, 573)
 confint(fit, level = 0.95)
+
+# tailprop
+2 * (1 - pt(abs(7.045554), df = 573))
+
+# Make dataset for validation
+D_test <- subset(D, (t == "2008-12-06" & houseId == 3) |
+  (t == "2009-02-22" & houseId == 5) |
+  (t == "2009-03-12" & houseId == 10) |
+  (t == "2009-04-01" & houseId == 17))
+
+# Predictions and 95% prediction intervals
+pred <- predict(fit, newdata = D_test,
+                interval = "prediction", level = 0.95)
+# Observed values and predictions
+cbind(id = D_test$houseId, Q = D_test$Q, pred)
